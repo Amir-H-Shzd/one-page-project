8bef32d323015141af60f8edb2dd7b802eb60557
Amir-H-Shzd <amirshir737@gmail.com>
2021-09-18 18:12:08 +0430

    Changes related to project implementation

--- onepage/urls.py
+++ onepage/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from onepage.settings import DEBUG
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,6 +23,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls'))
 ]
+
+# Changes related to project implementation
+
+# if DEBUG:
+#     urlpatterns += static(settings.STATIC_URL,
+#                           document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
